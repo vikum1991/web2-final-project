0bf83a3dda4516b5e46579a9a0b6261feca4e2b8
vikum1991 <131235760+vikum1991@users.noreply.github.com>
2023-07-25 22:16:17 +0900

    Add files via upload

---
+++ jsworld.js
@@ -0,0 +1,5 @@
+$(function(){
+  $('.flexslider').flexslider({
+    animation: "slide"
+  });
+});
---
+++ shop.js
@@ -0,0 +1,7 @@
+
+function detailedImageView1(imgLink) {
+
+  let firstDetailedItem = document.getElementById("firstDetailedItem");
+
+  firstDetailedItem.src ="https:" + imgLink;
+}
